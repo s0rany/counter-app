1e62a06efca514172e0235a30be651eb03f4fb9a
s0rany <sfc5954@psu.edu>
2025-02-23 12:59:11 -0500

    update

--- counter-app.js
+++ counter-app.js
@@ -69,6 +69,15 @@ export class CounterApp extends DDDSuper(I18NMixin(LitElement)) {
       .counter {
         font-size: var(--counter-app-label-font-size, var(--ddd-font-size-xxl));
       }
+      .buttons {
+        background-color: blue;
+      }
+      .buttons:hover {
+        background-color: red;
+      }
+      .buttons:focus {
+        background-color: green;
+      }
     `];
   }
 
@@ -100,6 +109,34 @@ export class CounterApp extends DDDSuper(I18NMixin(LitElement)) {
     this.count = 0;
   }
 
+  updated(changedProperties) {
+    if (super.updated) {
+      super.updated(changedProperties);
+    }
+    if (changedProperties.has('counter')) {
+      // do your testing of the value and make it rain by calling makeItRain
+    }
+  }
+
+  makeItRain() {
+    // this is called a dynamic import. It means it won't import the code for confetti until this method is called
+    // the .then() syntax after is because dynamic imports return a Promise object. Meaning the then() code
+    // will only run AFTER the code is imported and available to us
+    import("@haxtheweb/multiple-choice/lib/confetti-container.js").then(
+      (module) => {
+        // This is a minor timing 'hack'. We know the code library above will import prior to this running
+        // The "set timeout 0" means "wait 1 microtask and run it on the next cycle.
+        // this "hack" ensures the element has had time to process in the DOM so that when we set popped
+        // it's listening for changes so it can react
+        setTimeout(() => {
+          // forcibly set the poppped attribute on something with id confetti
+          // while I've said in general NOT to do this, the confetti container element will reset this
+          // after the animation runs so it's a simple way to generate the effect over and over again
+          this.shadowRoot.querySelector("#confetti").setAttribute("popped", "");
+        }, 0);
+      }
+    );
+  }
   /**
    * haxProperties integration via file reference
    */
